e3dfc8b32ea59e59af787a43419023e455589577
Jesuite <akhidenorjay1@gmail.com>
2023-04-01 21:58:57 +0100

    !!!

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ let open = () =>{
     menu.style.display = 'flex';
     document.querySelector('.menucover').style.display = 'flex'
     document.getElementById('close').style.display = 'flex'
-    document.getElementById('close').style.transition = 'ease-in 100ms'
+    document.getElementById('close').style.transition = 'ease-in 2s'
 }
 
 let close = () =>{
     menu.style.display = 'none';
     document.querySelector('.menucover').style.display = 'none'
     document.getElementById('close').style.display = 'none'
-    document.getElementById('close').style.transition = 'ease-out 100ms'
+    document.getElementById('close').style.transition = 'ease-out 2s'
 }
 
 openBtn.addEventListener("click", open);
