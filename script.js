b2e41e330d3d641babf602e8416eb035cdc3d293
Jesuite <akhidenorjay1@gmail.com>
2023-04-01 21:52:54 +0100

    hamburger button

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let menuBtn = document.querySelector(".dropdown")
+let openBtn = document.getElementById("open");
 let menu = document.querySelector('.dropmenu');
+let closeBtn = document.getElementById('close');
 
-let myFunction = () =>{
+let open = () =>{
     menu.style.display = 'flex';
     document.querySelector('.menucover').style.display = 'flex'
+    document.getElementById('close').style.display = 'flex'
 }
 
-menuBtn.addEventListener("click", myFunction);
+let close = () =>{
+    menu.style.display = 'none';
+    document.querySelector('.menucover').style.display = 'none'
+    document.getElementById('close').style.display = 'none'
+}
+
+openBtn.addEventListener("click", open);
+closeBtn.addEventListener("click", close);
