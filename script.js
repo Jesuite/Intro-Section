b86bc17a5a4630334c997e8dae43b8ad8ef4b74f
Jesuite <akhidenorjay1@gmail.com>
2023-04-02 21:17:22 +0100

    😁

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-let openBtn = document.getElementById("open");
-let menu = document.querySelector('.dropmenu');
-let closeBtn = document.getElementById('close');
+let primaryNavigation = document.querySelector("#primaryNavigation");
+let mobileNavToggle = document.querySelector('.mobileNavToggle');
+// let closeBtn = document.getElementById('close');
 
-let open = () =>{
+// let open = () =>{
     // menu.style.display = 'flex'
-    menu.style.right = '0%';
-    menu.style.transition = 'all ease-in 0.5s'
-    document.querySelector('.menucover').style.display = 'flex'
-    document.getElementById('close').style.transitionProperty = 'display'
-    document.getElementById('close').style.display = 'flex'
-    document.getElementById('open').style.display = 'none'
-}
+    // primaryNavigation.style.transform = 'translateX(0%)';
+    // menu.style.transition = 'all ease-in 0.5s'
+    // document.querySelector('.menucover').style.opacity = '0.7'
+    // document.querySelector('.menucover').style.transition = 'opacity 0.5s ease-in'
+    // document.getElementById('close').style.transitionProperty = 'display'
+    // document.getElementById('close').style.display = 'flex'
+    // document.getElementById('open').style.display = 'none'
+    // document.querySelector('.menucover').style.opacity = '70%'
+// }
 
-let close = () =>{
-    menu.style.right = '-65%';
-    menu.style.transition = 'all ease-out 0.5s'
-    document.querySelector('.menucover').style.display = 'none'
-    // document.getElementById('close').style.display = 'none'
-    document.getElementById('open').style.display = 'flex'
+// let close = () =>{
+//     menu.style.right = '-65vw';
+//     menu.style.transition = 'all ease-out 0.5s'
+//     document.querySelector('.menucover').style.opacity = '0'
+//     document.querySelector('.menucover').style.transition = 'opacity 0.5s ease-out'
+//     // document.querySelector('.menucover').style.display = 'none'
+//     // document.getElementById('close').style.display = 'none'
+//     document.getElementById('open').style.display = 'flex'
     
+// }
+
+let toggle = () =>{
+    const visibility = primaryNavigation.getAttribute('data-visible')
+
+    if(visibility === 'false'){
+        primaryNavigation.setAttribute('data-visible', 'true')
+        mobileNavToggle.setAttribute('data-close', 'true')
+        document.querySelector('.menucover').style.opacity = '0.7'
+        document.querySelector('.menucover').style.transition = 'opacity 0.5s ease-in'
+    }
+    else if(visibility === 'true'){
+        primaryNavigation.setAttribute('data-visible', 'false')
+        mobileNavToggle.setAttribute('data-close', 'false')
+        document.querySelector('.menucover').style.opacity = '0'
+        document.querySelector('.menucover').style.transition = 'opacity 0.5s ease-out'
+    }
+    // console.log(visibility)
 }
 
-openBtn.addEventListener("click", open);
-closeBtn.addEventListener("click", close);
+mobileNavToggle.addEventListener("click", toggle);
+// closeBtn.addEventListener("click", close);
